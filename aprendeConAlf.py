ea6fe518f1ef7907953905ff9a035e3b9fa486e7
Alexxpy1 <aortegapy@gmail.com>
2024-02-17 22:58:44 -0300

    Ejercicio 1 resuelto

---
+++ aprendeConAlf.py
@@ -0,0 +1,7 @@
+# Suma de los números de una lista
+
+entero = int(input("Ingrese un número entero: "))
+lista = list(range(entero))
+sumaLista = sum(lista)
+print(f'La suma de todos los números de esta lista {lista} es:\n{sumaLista}')
+
